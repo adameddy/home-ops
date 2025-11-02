d48e04097775c8a38d98e92772084a505026c94f
Adam Eddy <10340239+adameddy@users.noreply.github.com>
2025-11-02 08:18:43 -0700

    tf tests

--- terraform/proxmox/providers.tf
+++ terraform/proxmox/providers.tf
@@ -19,4 +19,5 @@ provider "proxmox" {
   pm_api_url = data.sops_file.proxmox_secrets.data["pm_api_url"]
   pm_api_token_id = data.sops_file.proxmox_secrets.data["pm_api_token_id"]
   pm_api_token_secret = data.sops_file.proxmox_secrets.data["pm_api_token_secret"]
+  pm_tls_insecure = true
 }
