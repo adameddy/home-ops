ed147af48fec9ad43e3dd014914253c76a4abb6d
Adam Eddy <10340239+adameddy@users.noreply.github.com>
2026-01-17 20:49:03 -0700

    perm test

--- terraform/talos/config.tf
+++ terraform/talos/config.tf
@@ -77,6 +77,9 @@ data "talos_machine_configuration" "worker" {
         proxy = {
           disabled = true
         }
+        apiServer = {
+          disablePodSecurityPolicy = true
+        }
       }
       machine = {
         features = {
